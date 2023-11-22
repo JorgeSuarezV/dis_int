69a6fdfb35f456081bb200411524d42d1091df3d
manu <manuel.hernandez@ing.austral.edu.ar>
2023-11-22 18:21:34 -0300

    changes V69

---
+++ src/components/OptionTransport/OptionTransport.stories.tsx
@@ -0,0 +1,18 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import OptionTransport from './index';
+import SmallBusSVG from '../../assets/icons/SmallBusSVG';
+
+const meta: Meta<typeof OptionTransport> = {
+   title: 'OptionTransport',
+   component: OptionTransport,
+   parameters: {
+      layout: 'centered',
+   },
+    tags: ['autodocs']
+};
+
+export default meta;
+
+export const Search: StoryObj<typeof OptionTransport> = {
+   render: (args) => <OptionTransport{...args} icon= {<SmallBusSVG/>} text={args.text ?? 'Transporte'}/>
+};

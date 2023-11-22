0977381845a0877855127c3cc25f5edcd6972872
PedroElizalde01 <pedro.elizalde@ing.austral.edu.ar>
2023-11-21 22:21:37 -0300

    iconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import PlusSVG from "../../assets/icons/PlusSVG";
+import { cva } from "class-variance-authority";
 
-
-function IconButton() {
-    return (
-        <div className={"flex justify-center items-center p-4 h-16 w-16 bg-primary-500 rounded-2xl"}>
-            <PlusSVG color={'white'}/>
-        </div>
-    )
+interface IconButtonProps{
+    icon?: ReactNode;
+    disabled?: boolean;
 }
 
-export default IconButton
+const variants = cva(["flex justify-center items-center p-4 h-16 w-16 rounded-2xl "],
+    {
+        variants: {
+            variant:{
+                enabled: ['bg-primary-500 cursor-pointer'],
+                disabled: ['bg-primary-200']
+            }
+        }
+    }
+);
+
+const IconButton = ({ icon, disabled }: IconButtonProps) => {
+    return (
+        <div className={variants({ variant: disabled ? 'disabled':'enabled' })} >
+            { icon ?? <PlusSVG color={'white'}/> }
+        </div>
+    )
+};
+
+export default IconButton;
